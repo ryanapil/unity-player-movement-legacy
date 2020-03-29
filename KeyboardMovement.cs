2cfe13df3b347361d2c520395978351b2b83aee7
Ryan <ryanapilbrow@gmail.com>
2020-03-29 15:04:00 +0100

    Add files via upload

---
+++ KeyboardMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour
+{
+
+    public CharacterController controller;
+
+    public float speed = 12f;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right * horizontal + transform.forward * vertical;
+
+        controller.Move(movement * speed * Time.deltaTime);
+    }
+
+}
